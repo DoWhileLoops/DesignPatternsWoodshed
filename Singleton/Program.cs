66f2e7d9e33d14ea583e046bfe48a79eaea857d2
Chris Clemons <chrisclemonstech@gmail.com>
2018-12-29 09:47:09 -0800

    add Singleton proj

---
+++ Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingletonClass
+    {
+
+        private string name;
+
+        private SingletonClass()
+        {
+            name = "I am a singleton class.";
+        }
+
+        private static Lazy<SingletonClass> instance = new Lazy<SingletonClass>(() => new SingletonClass());
+
+        public static SingletonClass Instance => instance.Value;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = SingletonClass.Instance;
+            var obj2 = SingletonClass.Instance;
+
+            Console.WriteLine("obj1 == obj 2 ?");
+            Console.WriteLine(obj1 == obj2);
+            Console.ReadKey();
+
+        }
+    }
+}
